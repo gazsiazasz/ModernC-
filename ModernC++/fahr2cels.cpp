fda36e38cffe3af7afed45272e7edac211982c63
Gajdos Gábor <gazsiazasz@gmail.com>
2017-10-04 18:58:58 +0200

    Fahrenheit to Celsius version 3

--- ModernC++/fahr2cels.cpp
+++ ModernC++/fahr2cels.cpp
@@ -13,7 +13,7 @@ int main()
 
     for ( fahr = -100; fahr <= 400; fahr += 25 )
     {
-        printf( "Fahr = %d\tCels = %d\n", fahr, 5./9.*(fahr-32) );
+        printf( "Fahr = %d\tCels = %f\n", fahr, 5./9.*(fahr-32) );
     }
     return 0;
 }
