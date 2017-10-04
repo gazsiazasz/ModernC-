e23b45681ebea2e82f4a3f6d854e5284efc9f3ee
Gajdos Gábor <gazsiazasz@gmail.com>
2017-10-04 18:42:29 +0200

    Initial Commit

---
+++ ModernC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ModernC++
+//
+//  Created by Gajdos Gábor on 2017. 10. 04..
+//  Copyright © 2017. Gajdos Gábor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
